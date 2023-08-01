c55fc9110f86cf80052e6459a469ce1536b7de46
Laura C. Balbachan dos Santos <balbachanlaura@gmail.com>
2023-08-01 17:10:31 -0300

    Initial Commit

---
+++ Mini01/Mini01App.swift
@@ -0,0 +1,17 @@
+//
+//  Mini01App.swift
+//  Mini01
+//
+//  Created by Laura C. Balbachan dos Santos on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Mini01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
